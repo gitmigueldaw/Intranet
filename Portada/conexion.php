4e196ccd95f7f0fee4cec8ba70ebd452038217e9
saggitgit <sagsag@hotmail.es>
2019-04-16 16:49:26 +0200

    Update conexion.php
    Lectura desde fichero.

--- Portada/conexion.php
+++ Portada/conexion.php
@@ -1,12 +1,31 @@
 <?php
 
+/* 
+ * Leyendo los datos del fichero "sitio.conf". Abajo está la clase que no lee del fichero.
+ * La ruta de "sitio.conf" es desde el directorio raíz, ya que este "Conexion.php" se invoca desde el
+ * index. Si se incluye este php en otro archivo que no esté en la raíz, puede fallar al no encontrar
+ * "sitio.conf". 
+ */
+
 class Conexion {
+    private $archivo; 
+    private $contenido; 
+    private $servidor;
+    private $usuario;
+    private $clave; 
+    private $bd; 
 
     private $con;
 
     function __construct() {
-        $this->con = new mysqli('localhost', 'root', 'Nohay2sin3', 'basedatos')
-                or die("No se ha podido conectar al servidor");
+        $this->setArchivo("Configuraciones/sitio.conf");
+        $this->setContenido(parse_ini_file($this->archivo, true));
+        $this->setServidor($this->contenido['servidor']);
+        $this->setUsuario($this->contenido['usuario_admin']);
+        $this->setClave($this->contenido['contrasenia_admin']);
+        $this->setBd($this->contenido['nombrebd']);
+
+        $this->con = new mysqli($this->servidor, $this->usuario, $this->clave, $this->bd) or die("No se ha podido conectar al servidor");
     }
 
     public function comprobarConexion($usuario, $pass) {
@@ -19,10 +38,55 @@ class Conexion {
             setcookie("rol", $codigo);
             setcookie("nombre", $nombre);
         } else {
-            $row = -1; 
-       }
-        
+            $row = -1;
+        }
         return $row;
     }
 
+    function setArchivo($archivo) {
+        $this->archivo = $archivo;
+    }
+
+    function setContenido($contenido) {
+        $this->contenido = $contenido;
+    }
+
+    function setServidor($servidor) {
+        $this->servidor = $servidor;
+    }
+
+    function setUsuario($usuario) {
+        $this->usuario = $usuario;
+    }
+
+    function setClave($clave) {
+        $this->clave = $clave;
+    }
+
+    function setBd($bd) {
+        $this->bd = $bd;
+    }
 }
+
+
+// ANTERIOR
+// class Conexion {
+//     private $con;
+//     function __construct() {
+// 	        $this->con = new mysqli('localhost', 'useradmin', 'phpp@sswd1819admin', 'basedatos') 
+//                or die("No se ha podido conectar al servidor");  // o  root y Nohay2sin3
+//     }
+//     public function comprobarConexion($usuario, $pass) {
+//         if ($stmt = $this->con->prepare("SELECT us_codrol, us_cuenta FROM usuarios WHERE us_cuenta = ? AND us_varios = ?")) {
+//             $stmt->bind_param("ss", $usuario, $pass);
+//             $stmt->execute();
+//             $stmt->bind_result($codigo, $nombre);
+//             $row = $stmt->fetch();
+//             setcookie("rol", $codigo);
+//             setcookie("nombre", $nombre);
+//         } else {
+//             $row = -1; 
+//        }
+//         return $row;
+//     }
+// }
